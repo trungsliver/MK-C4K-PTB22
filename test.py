491e874de5bab9903d53d39917123699645b2ae3
trungsliver <buiductrung.cva@gmail.com>
2024-09-27 18:11:08 +0700

    Denise

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 print('hello world')
+
+# Thông tin tài khoản làm BTVN
+# Thông tin truy cập Denise: 
+#     • Liên kết: https://base.mindx.edu.vn/login?callbackUrl=https://denise.mindx.edu.vn
+#     • Tài khoản: 23009231@mindx.edu.vn
+#     • Mật khẩu mặc định: Mindx@2024
