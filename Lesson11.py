84b1a03158885726166069e0daac1ffd9415a9a4
trungsliver <buiductrung.cva@gmail.com>
2024-10-18 16:27:06 +0700

    Lesson 11

--- Lesson11.py
+++ Lesson11.py
@@ -93,3 +93,13 @@ print(tong)
     # Cách mới
 tong2 = sum(item for item in arr2 if item%2==0)
 print(tong2)
+
+# -------------------------Luyện tập-----------------------
+# Bài 1: Nhập 2 thông tin: họ, tên. In ra màn hình tên đầy đủ
+
+# Bài 2: Nhập vào 1 xâu ký tự định dạng dd/mm/yyyy (01/08/2024)
+    # Tách ngày, tháng, năm và hiển thị ra màn hình
+
+# Bài 3: Nhập vào thông tin dạng username:password
+    # kiểm tra xem thông tin vừa nhập có trùng với thông tin có sẵn
+    # YC2: bắt người dùng nhập đến khi nào trùng username và password thì kết thúc
